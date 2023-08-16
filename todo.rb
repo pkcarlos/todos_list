259b50e160804c6e6bd69c89ce539c97211ce91d
pkcarlos <patricia.k.carlos@gmail.com>
2023-08-16 10:12:03 -0400

    adding main todo file

--- todo.rb
+++ todo.rb
@@ -5,7 +5,7 @@ require "tilt/erubis"
 
 configure do
   enable :sessions
-  set :session_secret, "b1b0ebf0cb4fc1eecf4db6b32003fe97a51bb36fa4dde9e448324dfb2f46e429"
+  set :session_secret, SecureRandom.hex(32)
   set :erb, :escape_html => true
 end
 
